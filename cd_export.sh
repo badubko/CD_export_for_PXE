184d194d78b15d21c8792cdc8aed136de5f02f89
badubko <badubko@gmail.com>
2023-01-26 01:12:14 -0300

    Evolucion 7

--- cd_export.sh
+++ cd_export.sh
@@ -65,9 +65,44 @@ ubuntu)
 	return
 	;;
 debian)
+    BOOT_STRING="live"
+
+    if [ -f  ${WHERE_TO_MOUNT}${FANTASY_NAME}/${BOOT_STRING}/vmlinuz* ]
+	then
+		VMLINUZ_STRING=$(ls ${WHERE_TO_MOUNT}${FANTASY_NAME}/${BOOT_STRING}/vmlinuz*)
+		VMLINUZ_STRING=${BOOT_STRING}/${VMLINUZ_STRING##*/}
+		echo "vmlinux string: ${VMLINUZ_STRING}"
+	else
+			echo "NO vmlinuz exists"
+			exit
+	fi
+	
+	if [ -f  ${WHERE_TO_MOUNT}${FANTASY_NAME}/${BOOT_STRING}/initrd* ]
+	then
+		INITRD_STRING=$(ls ${WHERE_TO_MOUNT}${FANTASY_NAME}/${BOOT_STRING}/initrd*)
+		INITRD_STRING=${BOOT_STRING}/${INITRD_STRING##*/}
+		echo "initrd string: ${INITRD_STRING}"
+		exit
+	else
+			echo "NO initrd exists"
+			exit
+	fi
+	
+	
+				
+	INITRD_STRING="${BOOT_STRING}/initrd"
+	MENU_STRING1="APPEND  root=/dev/nfs boot=${BOOT_STRING} netboot=nfs ip=dhcp "
+	MENU_STRING2=" nfsroot=${MY_SERVER_IP}:${WHERE_TO_MOUNT}${FANTASY_NAME} "
+	MENU_STRING3="initrd=${FANTASY_NAME}${INITRD_STRING}  no-quiet splash toram ---"
 	return
 	;;
 trisquel)
+    BOOT_STRING="casper"
+	VMLINUZ_STRING="/casper/vmlinuz"
+	INITRD_STRING="/casper/initrd"
+	MENU_STRING1="APPEND  root=/dev/nfs boot=${BOOT_STRING} netboot=nfs ip=dhcp "
+	MENU_STRING2=" nfsroot=${MY_SERVER_IP}:${WHERE_TO_MOUNT}${FANTASY_NAME} "
+	MENU_STRING3="initrd=${FANTASY_NAME}${INITRD_STRING}  no-quiet splash toram ---"
 	return
 	;;
 other)
