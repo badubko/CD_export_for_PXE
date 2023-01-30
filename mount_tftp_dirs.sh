612c6a305b90a81a785105a9e82c7e46b9822b00
badubko <badubko@gmail.com>
2023-01-29 22:12:51 -0300

    Changed variable name

--- mount_tftp_dirs.sh
+++ mount_tftp_dirs.sh
@@ -6,9 +6,9 @@ then
 	exit
 fi
 
-LIST_TO_UMOUNT=$(grep "/var/lib/tftpboot" </etc/fstab | cut -d " " -f 1)
+LIST_TO_MOUNT=$(grep "/var/lib/tftpboot" </etc/fstab | cut -d " " -f 1)
 
-for DIR in ${LIST_TO_UMOUNT}
+for DIR in ${LIST_TO_MOUNT}
 do
      #  echo $DIR 
 	 mount  -v  ${DIR}
