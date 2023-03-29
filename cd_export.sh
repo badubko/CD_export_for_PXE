c465585cb1cbe4ce5097ba6397e66ee5cec48343
badubko <badubko@gmail.com>
2023-03-29 18:05:33 -0300

    Changed string to reflect newer versions

--- cd_export.sh
+++ cd_export.sh
@@ -404,9 +404,9 @@ alma)
 	;;	
 
 mint)
-    BOOT_STRING="live"
-	VMLINUZ_STRING="/live/vmlinuz"
-	INITRD_STRING="/live/initrd.lz"
+    BOOT_STRING="casper"
+	VMLINUZ_STRING="/casper/vmlinuz"
+	INITRD_STRING="/casper/initrd.lz"
 	MENU_STRING1="APPEND  root=/dev/nfs boot=${BOOT_STRING} netboot=nfs ip=dhcp "
 	MENU_STRING2=" nfsroot=${MY_SERVER_IP}:${WHERE_TO_MOUNT}${FANTASY_NAME} "
 	MENU_STRING3="initrd=${DELTA}${FANTASY_NAME}${INITRD_STRING}  union=overlay  config components no-quiet  toram ---"
