c52249cf98d3df71dd0c968cbbf3b55cb893e1e9
badubko <badubko@gmail.com>
2023-03-30 12:09:04 -0300

    More  scripts for including

--- Support_Matrix/Supported/gparted.sh
+++ Support_Matrix/Supported/gparted.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
-BOOT_STRING="casper"
-	VMLINUZ_STRING="/casper/vmlinuz"
-	INITRD_STRING="/casper/initrd.lz"
+# gparted
+
+BOOT_STRING="live"
+	VMLINUZ_STRING="/live/vmlinuz"
+	INITRD_STRING="/live/initrd.img"
 	MENU_STRING1="APPEND  root=/dev/nfs boot=${BOOT_STRING} netboot=nfs ip=dhcp "
 	MENU_STRING2=" nfsroot=${MY_SERVER_IP}:${WHERE_TO_MOUNT}${FANTASY_NAME} "
 	MENU_STRING3="initrd=${DELTA}${FANTASY_NAME}${INITRD_STRING}  union=overlay  config components no-quiet  toram ---"
---
+++ Support_Matrix/Supported/pop_os.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# pop_os
+
+    BOOT_STRING="casper"
+	VMLINUZ_STRING="/casper/vmlinuz.efi"
+	INITRD_STRING="/casper/initrd.gz"
+	MENU_STRING1="APPEND  root=/dev/nfs boot=${BOOT_STRING} netboot=nfs ip=dhcp "
+	MENU_STRING2=" nfsroot=${MY_SERVER_IP}:${WHERE_TO_MOUNT}${FANTASY_NAME} "
+	MENU_STRING3="initrd=${DELTA}${FANTASY_NAME}${INITRD_STRING}  no-quiet  toram ---"
